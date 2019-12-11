16acf2afd5094fc6d603a2d049033517eb633ba7
Ryan de Kwaadsteniet <ryanrdk@gmail.com>
2019-12-11 14:24:08 +0200

    buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <Button variant='contained' color='primary'>
+          Create Room
+        </Button>
+        <br></br>
+        <Button variant='contained' color='secondary'>
+          Join Room
+        </Button>
       </header>
     </div>
   );
