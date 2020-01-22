7e86c46c1d576baf5c5b4f562c183b7a7ea1a310
Ryan de Kwaadsteniet <ryanrdk@gmail.com>
2020-01-22 16:08:28 +0200

    ssl

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 const sslRedirect = require('./src/authentication/heroku-ssl-redirect/');
+var enforce = require('express-sslify');
 
 // app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
@@ -12,9 +13,10 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.use(function(req, res, next) {
-  if (req.headers['x-forwarded-proto'] != 'https')
-    res.redirect(['https://', req.get('Host'), req.url].join(''));
-  else next();
-});
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+// app.use(function(req, res, next) {
+//   if (req.headers['x-forwarded-proto'] != 'https')
+//     res.redirect(['https://', req.get('Host'), req.url].join(''));
+//   else next();
+// });
 app.listen(port);
