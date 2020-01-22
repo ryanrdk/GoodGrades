82b5c2c2f466f329ee1b34183e1a7cdf2ccc4a00
Ryan de Kwaadsteniet <ryanrdk@gmail.com>
2020-01-22 14:42:47 +0200

    import quotation marks fix

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-const sslRedirect = require(‘heroku-ssl-redirect’);
+const sslRedirect = require('heroku-ssl-redirect');
+
 // app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
