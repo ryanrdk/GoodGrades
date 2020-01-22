c3684cd192d2e8ebbcf0104ecef496d25d5ed9de
Ryan de Kwaadsteniet <ryanrdk@gmail.com>
2020-01-22 15:15:05 +0200

    import custom ssl redirect package

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-const sslRedirect = require('heroku-ssl-redirect');
+const sslRedirect = require('./src/authentication/heroku-ssl-redirect/');
 
 // app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
@@ -12,22 +12,24 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.use(function(environments, status) {
-  environments = environments || ['production'];
-  status = status || 302;
-  return function(req, res, next) {
-    if (environments.indexOf(process.env.NODE_ENV) >= 0) {
-      if (req.headers['x-forwarded-proto'] != 'https') {
-        res.redirect(
-          status,
-          'https://' + req.hostname + req.originalUrl + '/login'
-        );
-      } else {
-        next();
-      }
-    } else {
-      next();
-    }
-  };
-});
+app.use(sslRedirect());
+// app.use((environments, status) => {
+//   environments = environments || ['production'];
+//   status = status || 302;
+//   console.log('https://' + req.hostname + req.originalUrl + '/login');
+//   return function(req, res, next) {
+//     if (environments.indexOf(process.env.NODE_ENV) >= 0) {
+//       if (req.headers['x-forwarded-proto'] != 'https') {
+//         res.redirect(
+//           status,
+//           'https://' + req.hostname + req.originalUrl + '/login'
+//         );
+//       } else {
+//         next();
+//       }
+//     } else {
+//       next();
+//     }
+//   };
+// });
 app.listen(port);
