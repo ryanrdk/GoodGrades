2fd0f2fd7b065d1555e87bd3264d3ab5d5cb4b10
jde-agr <jde-agr@student.wethinkcode.co.za>
2020-01-23 12:37:12 +0200

    Simple in-app notifications

--- src/App.js
+++ src/App.js
@@ -9,10 +9,15 @@ import { BookingsView } from './views/BookingsView';
 import { HomeView } from './views/HomeView';
 import SchedulerView from './views/SchedulerView';
 import SwipeableRoutes from 'react-swipeable-routes';
+import moment from 'moment';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { isMobile } from 'react-device-detect';
 
 function App() {
   const [user, setUser] = useState({});
   const [booked, setBooked] = useState(null);
+  const [notifications] = useState([]);
 
   const getBookings = () => {
     var targetUrl = user.type === "tutor" ?
@@ -24,6 +29,14 @@ function App() {
       .then(data => {
         console.log({data})
         setBooked(data);
+        data.forEach(elem => {
+          let isDuplicateNotification = notifications.some(elem2 => {
+            return (elem.tutor === elem2.booking.tutor && elem.start_time === elem2.booking.start_time)
+          });
+          if (!isDuplicateNotification) {
+            notifications.push({ time_type: 1, booking: elem })
+          }
+        })
         return data;
       })
       .catch(e => {
@@ -32,14 +45,47 @@ function App() {
       });
   }
 
+  const sendNotification = (elem, time_diff) => {
+    toast(<div>
+      {time_diff + 1} min till session starts<br/>
+      {user.type === "tutor" ? `Student : ${elem.booking.students[0].username}` : `Tutor : ${elem.booking.tutor_username}`}<br/>
+      {/* Tutor : {elem.booking.tutor_username}<br/> */}
+      {/* Student : { elem.booking.students[0].username }<br/> */}
+      {/* Date : { moment(elem.booking.start_time).format("dddd, MMM DD") }<br/> */}
+      Time : { moment(elem.booking.start_time).format("hh:mm a") } - { moment(elem.booking.end_time).format("hh:mm a") }<br/>
+      </div>
+      , {
+        autoClose: 10000,
+        type: toast.TYPE.INFO,
+      })
+  }
+
   useEffect(()=>{
     if (!booked && user.unique_id) {
       getBookings()
     }
+    const interval = setInterval(() => {
+      console.log("Notifs", notifications, user)
+      notifications.forEach((elem, indx, arr) => {
+        let time_diff = moment(elem.booking.start_time).diff(Date.now(), 'minutes')
+        if (elem.time_type !== 2 && time_diff > 30 && time_diff < 60) {
+          sendNotification(elem, time_diff);
+          arr[indx].time_type = 2;
+        } else if (elem.time_type !== 3 && time_diff > 15 && time_diff < 30) {
+          sendNotification(elem, time_diff);
+          arr[indx].time_type = 3;
+        } else if (elem.time_type !== 4 && time_diff > 0 && time_diff < 15) {
+          sendNotification(elem, time_diff);
+          arr[indx].time_type = 4;
+        }
+      });
+      }, 5000);
+    return () => clearInterval(interval);
   })
 
   return (
     <BrowserRouter>
+    <ToastContainer position={isMobile ? "top-center" : "bottom-left"}/>
       <div>
         <Route
           path='/login'
