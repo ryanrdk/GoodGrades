5f94aa5e80167f8cfd04574f05865fad67731f00
Onex101 <xeno@zarobi.co.za>
2020-01-31 09:27:26 +0200

    Still have no idea what im doing

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -10,6 +10,64 @@ firebase.initializeApp({
 	messagingSenderId: "198987621325",
 	appId: "1:198987621325:web:efc84e13fd9e33daffda84",
 	measurementId: "G-63JQF718BE"
-  });
+});
 
 const messaging = firebase.messaging();
+
+messaging.usePublicVapidKey("BFmfR5eX_gbssL9B0PPK31MA4RKBq9iLFzUY9MD9B9sL7jovYUhH4aAcgJKWDCBrPUwfjUNx_FcRzaNDELSuxyU");
+messaging.useServiceWorker('./worker.js')
+
+messaging.setBackgroundMessageHandler(function (payload) {
+	console.log("Message has been handled")
+	const notificationTitle = payload.data.title;
+	const notificationOptions = {
+	  body: payload.data.body,
+	  icon: payload.data.icon
+	};
+	return self.registration.showNotification(notificationTitle,
+	  notificationOptions);
+});
+
+messaging.showToken = (token) => {
+	console.log(token)
+
+}
+
+messaging.getToken().then((currentToken) => {
+	if (currentToken) {
+		messaging.sendTokenToServer(currentToken);
+		messaging.updateUIForPushEnabled(currentToken);
+	} else {
+	  // Show permission request.
+	  console.log('No Instance ID token available. Request permission to generate one.');
+	  // Show permission UI.
+	  messaging.updateUIForPushPermissionRequired();
+	  messaging.setTokenSentToServer(false);
+	}
+  }).catch((err) => {
+	console.log('An error occurred while retrieving token. ', err);
+	messaging.showToken('Error retrieving Instance ID token. ', err);
+	setTokenSentToServer(false);
+});
+
+messaging.onTokenRefresh(() => {
+	messaging.getToken().then((refreshedToken) => {
+		console.log('Token refreshed.');
+		// Indicate that the new Instance ID token has not yet been sent to the
+		// app server.
+		messaging.setTokenSentToServer(false);
+		// Send Instance ID token to app server.
+		messaging.sendTokenToServer(refreshedToken);
+		// ...
+	}).catch((err) => {
+		console.log('Unable to retrieve refreshed token ', err);
+		messaging.showToken('Unable to retrieve refreshed token ', err);
+	});
+});
+
+messaging.getInitialNotification((message) => { console.log("Got getInitialNotification "+JSON.stringify(message)); });
+
+messaging.onMessage(function(payload) {
+	console.log("Message received. ", payload);
+	   // ...
+ });
--- public/worker.js
+++ public/worker.js
@@ -1,39 +1,6 @@
 var CACHE_NAME = 'good-grades';
 var urlsToCache = ['/', '/login', '/bookings', '/schedular'];
 
-// importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js");
-// importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js");
-
-// firebase.initializeApp({
-// // Project Settings => Add Firebase to your web app
-//   messagingSenderId: "198987621325"
-// });
-
-// const messaging = firebase.messaging();
-
-// messaging.setBackgroundMessageHandler(function(payload) {
-//   const promiseChain = clients
-//     .matchAll({
-//       type: "window",
-//       includeUncontrolled: true
-//     })
-//     .then(windowClients => {
-//       for (let i = 0; i < windowClients.length; i++) {
-//         const windowClient = windowClients[i];
-//         windowClient.postMessage(payload);
-//       }
-//     })
-//     .then(() => {
-//       return registration.showNotification("my notification title");
-//     });
-//   return promiseChain;
-// });
-
-// self.addEventListener('notificationclick', function(event) {
-//   // do what you want
-//   // ...
-// });
-
 // Install a service worker
 self.addEventListener('install', event => {
   // Perform install steps
